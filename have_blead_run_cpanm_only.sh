325968d892499a3bc112df793bae61ecc1204928
James E Keenan <jkeenan@cpan.org>
2014-06-23 11:29:12 -0400

    If testing a second distro against the same version of blead, don't reinstall blead.

---
+++ have_blead_run_cpanm_only.sh
@@ -0,0 +1,33 @@
+# Set up to debug a CPAN module using a development Perl, such as blead.
+# Run this shell script while cd'd to the directory containing the Perl source
+
+source /home/jkeenan/gitwork/test-against-blead/config-test-against-blead.sh
+
+set -x
+
+# Install a fresh copy of cpanm under module testing directory
+wget --no-check-certificate --output-document=$MODULE_TEST_DIR/bin/cpanm --quiet http://bit.ly/cpanm 
+
+
+# $MODULE is the module being tested.  I like to just add ones to the end, to
+# keep a record of what I've worked on.
+# Note: Issues with these modules have not necessarily been resolved.
+#MODULE=AnyEvent::Fork
+#MODULE=Crypt::OpenSSL::X509
+#MODULE=Compress::Bzip2
+#MODULE=Wx
+#MODULE=CDB_File
+#MODULE=IP::World
+MODULE=Text::CSV::Hashify
+
+# Get it and its dependencies
+chmod 0755 $MODULE_TEST_DIR/bin/cpanm
+$MODULE_TEST_DIR/bin/cpanm --installdeps $MODULE
+
+set +x
+
+echo "About to cd a work space directory" >&2
+echo "From there, run '$MODULE_TEST_DIR/bin/perl Makefile.PL'" >&2
+echo "And then, probably 'make test'" >&2
+
+$MODULE_TEST_DIR/bin/cpanm --look $MODULE
