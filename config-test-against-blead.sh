c5de55cc21fbfe8b97f5bd4a295e6bd0d0b89f4b
James E Keenan <jkeenan@cpan.org>
2014-06-23 08:55:24 -0400

    Move variable assignments out to configuration file.

---
+++ config-test-against-blead.sh
@@ -0,0 +1,10 @@
+# In case you have this set, it shouldn't be for these purposes
+export PERL5OPT=
+# Set to a suitable number for parallelism on your system
+export TEST_JOBS=8
+export USER=jkeenan
+MODULE_TEST_SUBDIR=testing/blead
+# Set to a suitable directory where you want the
+# devel perl installed, for the purpose of testing
+export MODULE_TEST_DIR=/home/$USER/$MODULE_TEST_SUBDIR
+
--- test-against-blead.sh
+++ test-against-blead.sh
@@ -1,16 +1,9 @@
 # Set up to debug a CPAN module using a development Perl, such as blead.
 # Run this shell script while cd'd to the directory containing the Perl source
 
+source /home/jkeenan/gitwork/test-against-blead/config-test-against-blead.sh
+
 set -x
-# In case you have this set, it shouldn't be for these purposes
-PERL5OPT=
-# Set to a suitable number for parallelism on your system
-TEST_JOBS=8
-USER=jkeenan
-MODULE_TEST_SUBDIR=testing/blead
-# Set to a suitable directory where you want the
-# devel perl installed, for the purpose of testing
-MODULE_TEST_DIR=/home/$USER/$MODULE_TEST_SUBDIR
 
 # Add whatever other Configure options you need.  To use gdb, I like to have
 # -DDEBUGGING=both
@@ -24,21 +17,21 @@ make -j${TEST_JOBS} install
 wget --no-check-certificate --output-document=$MODULE_TEST_DIR/bin/cpanm --quiet http://bit.ly/cpanm 
 
 
-# $module is the module being tested.  I like to just add ones to the end, to
+# $MODULE is the module being tested.  I like to just add ones to the end, to
 # keep a record of what I've worked on.
 # Note: Issues with these modules have not necessarily been resolved.
-#module=AnyEvent::Fork
-#module=Crypt::OpenSSL::X509
-#module=Compress::Bzip2
-#module=Wx
-#module=CDB_File
-#module=IP::World
-#module=Text::CSV::Hashify
-module=List::Compare
+#MODULE=AnyEvent::Fork
+#MODULE=Crypt::OpenSSL::X509
+#MODULE=Compress::Bzip2
+#MODULE=Wx
+#MODULE=CDB_File
+#MODULE=IP::World
+MODULE=Text::CSV::Hashify
+#MODULE=List::Compare
 
 # Get it and its dependencies
 chmod 0755 $MODULE_TEST_DIR/bin/cpanm
-$MODULE_TEST_DIR/bin/cpanm --installdeps $module
+$MODULE_TEST_DIR/bin/cpanm --installdeps $MODULE
 
 set +x
 
@@ -46,4 +39,4 @@ echo "About to cd a work space directory" >&2
 echo "From there, run '$MODULE_TEST_DIR/bin/perl Makefile.PL'" >&2
 echo "And then, probably 'make test'" >&2
 
-$MODULE_TEST_DIR/bin/cpanm --look $module
+$MODULE_TEST_DIR/bin/cpanm --look $MODULE
